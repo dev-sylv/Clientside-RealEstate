44a8e13f1045d5cd27316825686d6aaf8ea92f0c
nicsylvia <nicsylvia15f@gmail.com>
2023-02-03 14:31:36 -0800

    saved perfect home

--- src/Components/HomeScreen/PerfectHome.tsx
+++ src/Components/HomeScreen/PerfectHome.tsx
@@ -5,7 +5,9 @@ const PerfectHome = () => {
   return (
     <div>
         <Container>
-            <Wrapper></Wrapper>
+            <Wrapper>
+                <h1>Let's find home that's <br /> perfect for you</h1>
+            </Wrapper>
         </Container>
     </div>
   )
@@ -17,5 +19,19 @@ const Container = styled.div`
     width: 100%;
     background-color: #F8F9FA;
     padding: 50px 0px 50px 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    width: 95%;
+    background-color: red;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    color: #005555;
+    h1{
+        text-align: center;
+    }
+`;
--- src/Components/HomeScreen/WhiteComp.tsx
+++ src/Components/HomeScreen/WhiteComp.tsx
@@ -23,6 +23,7 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    color: #005555;;
 `;
 const Wrapper = styled.div`
     p{
