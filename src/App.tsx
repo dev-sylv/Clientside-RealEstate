3ba196a11d0252dad87a39a4cf96ccf761d17b2c
nicsylvia <nicsylvia15f@gmail.com>
2023-02-03 20:16:58 -0800

    saved global fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import AllRoutes from './Components/AllRoutes/AllRoutes'
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
+import { GlobalStyle } from './GlobalFonts/GlobalFonts'
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <Header />
         <AllRoutes />
       <Footer />
