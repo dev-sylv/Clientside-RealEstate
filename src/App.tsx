cad96890e212b68fd08c8a37584da56ad5c57cd4
nicsylvia <nicsylvia15f@gmail.com>
2023-03-12 23:43:14 -0700

    saved complete app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AllRoutes from './Components/AllRoutes/AllRoutes'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+import { GlobalStyle } from './GlobalFonts/GlobalFonts'
+
+const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      <Header />
+        <AllRoutes />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
